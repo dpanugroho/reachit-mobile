54d9cf06eae175660d8b9ab3eb52f0443efb590e
Dwi Prasetyo Adi Nugroho <dwiprasetyo.an@gmail.com>
2019-05-11 09:38:33 +0200

    Add Offer Structure

--- lib/Offer.dart
+++ lib/Offer.dart
@@ -4,24 +4,46 @@ import 'package:flutter/material.dart';
 class Offer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
         children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.shopping_cart),
-            title: Text('Strawberry'),
+          Text("FOR YOU", style: TextStyle(fontSize: 20),),
+          Card(
+            child: new Container(
+              width: 360.0,
+              height: 160.0,
+              padding: new EdgeInsets.all(32.0),
+              child: new Column(
+                children: <Widget>[
+                  new Text('Hello World'),
+                  new Text('How are you?')
+                ],
+              ),
+            ),
           ),
-          ListTile(
-            leading: Icon(Icons.shopping_cart),
-            title: Text('Coffee'),
-          ),
-          ListTile(
-            leading: Icon(Icons.shopping_cart),
-            title: Text('Tea'),
+          Text("BY CATEGORY", style: TextStyle(fontSize: 20),),
+          Container(
+            height: 300.0,
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.shopping_cart),
+                  title: Text('Bathroom Supplies'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.shopping_cart),
+                  title: Text('Fruits and Vegetables'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.shopping_cart),
+                  title: Text('Bread'),
+                ),
+              ],
+            ),
           ),
         ],
       ),
-
     );
   }
 }
